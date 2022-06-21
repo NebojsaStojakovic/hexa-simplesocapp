4142d94ae4f14104c87a3d0f580814105c82e841
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-06-21 16:30:59 +0200

    commit2

---
+++ js/Post.js
@@ -0,0 +1,32 @@
+class Post {
+  post_id = "";
+  post_content = "";
+  user_id = "";
+  likes = "";
+  api_url = "https://62b18f29196a9e9870380ad8.mockapi.io";
+
+  async create() {
+    let session = new Session();
+    session_id = session.getSession();
+
+    let data = {
+      user_id: session_id,
+      content: this.post_content,
+      likes: 0,
+    };
+
+    data = JSON.stringify(data);
+
+    let response = await fetch(this.api_url + "/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+
+    data = await response.json();
+
+    return data;
+  }
+}
